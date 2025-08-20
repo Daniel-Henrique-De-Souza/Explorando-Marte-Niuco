9eb8c1f0aa302ba756a211716ff5d6af63231849
Daniel-Henrique-De-Souza <109430684+Daniel-Henrique-De-Souza@users.noreply.github.com>
2025-08-20 08:57:55 -0300

    Configurado e validado testes unitários

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
 				test: {
 					name: 'server',
 					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					include: ['tests/**/*.{test,spec}.{js,ts}'],
+					//exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
 				}
 			}
 		]
