4eb4dbd3272319da54cea9acafe14724950c5e53
Daniel-Henrique-De-Souza <109430684+Daniel-Henrique-De-Souza@users.noreply.github.com>
2025-08-20 08:31:52 -0300

    Criado estrutura básica da tela inicial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="main">
+    <div class="plain">Visualização</div>
+    <div class="left-panel">
+        <div class="mission-control">Controle da Missão</div>
+        <div class="mission-report">Relatório da Missão</div>
+    </div>
+</div>
+
+<style>
+    .main {
+        width: 100vw;
+        height: 100vh;
+        background: gray;
+        display: flex;
+    }
+
+    .left-panel {
+        width: 50%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .plain {
+        width: 50%;
+        height: 100%;
+        background: #222;
+    }
+
+    .mission-control {
+        width: 100%;
+        height: 50%;
+        background: #444;
+    }
+
+    .mission-report {
+        width: 100%;
+        height: 50%;
+        background: #555;
+    }
+</style>
