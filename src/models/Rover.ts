ecc703a05b85910ae2c2d1eeb298964bd88a6b3c
Daniel-Henrique-De-Souza <109430684+Daniel-Henrique-De-Souza@users.noreply.github.com>
2025-08-20 16:47:35 -0300

    Especificado tipo string para comandos na classe rover

--- src/models/Rover.ts
+++ src/models/Rover.ts
@@ -3,7 +3,7 @@ export default class Rover {
     x: number;
     y: number;
     direction: string = "N";
-    commands: [] = [];
+    commands: string[] = [];
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
